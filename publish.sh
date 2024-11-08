644a23ce4313c5c387547ca2e2a6f5b3a60c0a56
Dirceu Belém <dirceubelem@gmail.com>
2024-11-08 09:37:27 -0300

    Publish

---
+++ publish.sh
@@ -0,0 +1,5 @@
+./gradlew clean assemble
+./gradlew clean publish
+mkdir /opt/tomcat/webapps/libs/chat/onthego/onthego-helperprocess/$1
+cp build/libs/*.jar /opt/tomcat/webapps/libs/chat/onthego/onthego-helperprocess/$1/onthego-helperprocess-$1.jar
+cp build/publications/mavenJava/pom-default.xml /opt/tomcat/webapps/libs/chat/onthego/onthego-helperprocess/$1/onthego-helperprocess-$1.pom
